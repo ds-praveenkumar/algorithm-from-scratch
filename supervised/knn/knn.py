b7ece6e2e0a9fa092d9371a302eef0565226ef61
ds-praveenkumar <praveenkr2208@gmail.com>
2020-09-12 00:09:31 +0530

    knn algorithm initial commit

---
+++ supervised/knn/knn.py
@@ -0,0 +1,14 @@
+# knn.py
+
+class KNearestNeighbour:
+"""
+    This class implements k Nearest Neighbour
+"""
+
+    def __init__(self, n_neighbours=2):
+    """
+        initializes k
+    """
+    self.n_neighbours = n_neighbours
+    
+
