02457d3aaad399367d7cadaecfb089dcd6284b0e
ds-praveenkumar <praveenkr2208@gmail.com>
2020-09-12 14:49:40 +0530

    add another method

--- supervised/knn/test_knn.py
+++ supervised/knn/test_knn.py
@@ -4,17 +4,27 @@ from knn import KNearestNeighbour
 
 class TestKNN(unittest.TestCase):
     """
-        peerforms unit test for class KNearestNeighbour
+        performs unit test for class KNearestNeighbour
     """
     def test_calculate_distance(self):
         """
 
         """
-        point = (0,4)
-        test_point = (0,1)
-        dist = KNearestNeighbour.calculate_distance(point=point, test_point=test_point)
-        self.assertEqual(dist, 3)
+        point = 0,1
+        test_point = 0,2
+        dist = KNearestNeighbour.calculate_distance(self, point=point, test_point=test_point)
+        self.assertEqual(dist, 1)
 
+    def test_predict(self):
+        """
+            performs unit test for predict
+        """
+        knn = KNearestNeighbour(2)
+        knn.fit([1, 2, 3769, 67, 75, 7, 5 , 6])
+        knn.predict(5)
+        res = len(KNearestNeighbour.predict(self, 5))
+        self.assertEqual(res, 5)
+        pass
 
 
 if __name__ == "__main__":
