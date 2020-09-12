97cd8cd363c73c78344bc80d06a8a5475627e27b
ds-praveenkumar <praveenkr2208@gmail.com>
2020-09-12 14:42:06 +0530

    predict method added

--- supervised/knn/knn.py
+++ supervised/knn/knn.py
@@ -1,5 +1,6 @@
 # knn.py
 import numpy as np
+import operator
 
 class KNearestNeighbour:
     """
@@ -11,26 +12,25 @@ class KNearestNeighbour:
         """
         self.n_neighbours = n_neighbours
         self.data_points_len = None
-        self.data = None
+        self.data_set = None
 
-    def calculate_distance(method="euclidean", point=(0,0), test_point=(0,0)):
+    def calculate_distance(self, point=tuple, test_point=tuple, method="euclidean" ):
         """
-            distance =  
+            distance =  sqrt( (x1-y1)^2 + (x2-y2)^2 )
         """
+        dist = None
         if method == "euclidean":
             sub1, sub2 = np.subtract(point[0], test_point[0]), np.subtract(point[1], test_point[1])
-            add = np.add(sub1**2,sub2**2)
-            dist = np.sqrt(add)
+            squared = np.add(sub1**2, sub2**2)
+            dist = np.sqrt(squared)
             return dist
-
-
 
 
     def fit(self, data=list):
         """
-            This trains the data 
+            This fits the data to the data to the algorithm 
         """
-        self.data = data
+        self.data_set = data.copy()
         self.data_points_len = len(data)
         if self.n_neighbours > self.data_points_len:
             raise Exception(f"n_neighbours greater than number of data points: found clusters:{self.n_neighbours} data points length {self.data_points_len}'")
@@ -44,12 +44,19 @@ class KNearestNeighbour:
             random_k[index] = (_random_k)
 
         
-    def predict(self, test_data=list):
+    def predict(self, test_point):
         """
 
         """
-        
+        dist_list = []
+        for val in self.data_set:
+            distance = self.calculate_distance((0,val), (0,test_point))  
+            dist_list.append((val, distance)) 
+            dist_list.sort(key=operator.itemgetter(1))
+        return dist_list[0][0] 
 
 if __name__ == "__main__":
     knn = KNearestNeighbour(2)
-    knn.fit([1,2,3769,67, 75])
+    knn.fit([1, 2, 3769, 67, 75, 7, 5 , 6, 7, 8])
+    test_point = 3
+    print(f"test label:{test_point}, predicted label: {knn.predict(test_point)}")
